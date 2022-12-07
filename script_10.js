478e82880f19d72317f49a20523281b20bbbe398
Сливинский Максим <SlivinskiyMS@sielom.local>
2022-12-07 12:32:48 +0500

    Исправление 3.0

--- script_10.js
+++ script_10.js
@@ -1,16 +1,7 @@
 //Дан массив с числами. Найдите среднее арифметическое его элементов.
-const count = () => {
-    let i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let y = 0
-    let j = 0
-    let array = []
-    const a = `${i.length - 1}`
-    while (y <= a) {
-      j = j + i[y] / a
-      y++
-    }
-    array.push(j)
-    console.log (array)
-    return y
-  }
-  count()
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let result = 0
+for (let i = 0; i <= array.length - 1; i++) {
+  result = result + array[i] / array.length
+}
+console.log(result)
--- script_9.js
+++ script_9.js
@@ -1,16 +1,7 @@
 //Дан массив с числами. С помощью цикла найдите сумму квадратов элементов этого массива.
-const count = () => {
-    let i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let y = 0
-    let j = 0
-    let array = []
-    const a = `${i.length - 1}`
-    while (y <= a) {
-      j = j + i[y]**2
-      y++
-    }
-    array.push(j)
-    console.log (array)
-    return y
-  }
-  count()
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let result = 0
+for (let i = 0; i <= array.length - 1; i++) {
+  result = array[i]**2 + result
+}
+console.log(result)
